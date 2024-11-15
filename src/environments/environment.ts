a53dc45333e601a2176fca3575de9661df22f150
ValXux <valenferxux@gmail.com>
2024-11-15 00:14:57 -0500

    Comentar /dist en .gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:8080/api/v1',
+    baseUrl: 'https://healthybites-api-latest.onrender.com/api/v1'
+    //paypalReturnUrl: 'http://localhost:4200/paypal/',
 };

30d437b2efd04c266122ae922578b7cffdaec16b
ValXux <valenferxux@gmail.com>
2024-11-28 10:53:16 -0500

    agregando url paypal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     //baseUrl: 'http://localhost:8080/api/v1',
     //paypalReturnUrl: 'http://localhost:4200/cliente/perfil',
     baseUrl: 'https://healthybites-api-latest.onrender.com/api/v1',
-    paypalReturnUrl: 'https://healthybites-client-latest.onrender.com/cliente/perfil'
+    paypalReturnUrl: 'https://healthy-bites-v1.netlify.app//cliente/perfil'
 };

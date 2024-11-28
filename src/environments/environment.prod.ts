a5b1af6ceb869340b931eb1a3c4d8405545dd64a
ValXux <valenferxux@gmail.com>
2024-11-28 10:40:07 -0500

    actualizar configuraciones de entorno para producción; cambiar URLs de API y PayPal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:8080/api/v1',
-    paypalReturnUrl: 'http://localhost:4200/cliente/perfil',
+    //baseUrl: 'http://localhost:8080/api/v1',
+    //paypalReturnUrl: 'http://localhost:4200/cliente/perfil',
+    baseUrl: 'https://healthybites-api-latest.onrender.com/api/v1',
+    paypalReturnUrl: 'https://healthybites-client-latest.onrender.com/cliente/perfil'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:8080/api/v1',
-    paypalReturnUrl: 'http://localhost:4200/cliente/perfil',
-    //baseUrl: 'https://healthybites-api-latest.onrender.com/api/v1'
+    production: true,
+    //baseUrl: 'http://localhost:8080/api/v1',
+    //paypalReturnUrl: 'http://localhost:4200/cliente/perfil',
+    baseUrl: 'https://healthybites-api-latest.onrender.com/api/v1',
+    paypalReturnUrl: 'https://healthybites-client-latest.onrender.com/cliente/perfil'
 };
